26d685c166c092f2bb6e83e9a5f81e829e06547e
JoChiHO <gh30212@gmail.com>
2023-12-30 15:31:55 +0900

    Readme update

--- Basic_DS/Array_List/DS_Array.java
+++ Basic_DS/Array_List/DS_Array.java
@@ -3,23 +3,22 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 import java.util.Arrays;
 
-
 public class DS_Array {
 
-    class PrimeCount {
+    static class PrimeCount {
 
-        PrimeCount() throws Exception{
+        PrimeCount() throws Exception {
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             int number = Integer.parseInt(br.readLine());
             boolean[] a = new boolean[number + 1];
-            Arrays.fill(a, true); 
+            Arrays.fill(a, true);
 
-            a[0] = a[1] = false;  // 0과 1은 소수가 아니므로 false로 설정
+            a[0] = a[1] = false;
 
             for (int i = 2; i * i <= number; i++) {
                 if (a[i]) {
                     for (int j = i * i; j <= number; j += i) {
-                        a[j] = false;  // i의 배수들은 소수가 아니므로 false로 설정
+                        a[j] = false;
                     }
                 }
             }
@@ -31,7 +30,7 @@ public class DS_Array {
                 }
             }
 
-            System.out.println("Prime number: " + count);
+            System.out.println("소수의 개수: " + count);
         }
     }
 
@@ -44,13 +43,15 @@ public class DS_Array {
 
             for (int j = 0; j < s; j++) {
                 int data = Integer.parseInt(st.nextToken());
-                System.out.println(data);
+                System.out.print(data + " ");
             }
+
+            System.out.println();
         }
     }
 
     public static void main(String[] args) throws Exception {
-        DS_Array dsArray = new DS_Array(); 
-        dsArray.answer();  
+        DS_Array.PrimeCount pc = new DS_Array.PrimeCount();
+        pc.answer();
     }
 }
--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Algorithms and Data Structures for SW Design
 
 Data Structures: Basics for Algorithms  
+
+-23/12/30 Java를 통한 PS 코드 작성시 기본적인 문제점을 발견, 특히 라이브러리 활용 부분에서 문제가 있음을 발견해 
+Youtube Gaurav Sen 의 Comtetitive Programming In Java를 참고하기로함
